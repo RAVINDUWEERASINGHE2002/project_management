025f95febfd992f6a83f11e2bb8f8162607e2c04
Ravindu Weerasinghe <ravinduweerasinghe5739422@gmail.com>
2026-04-20 10:02:54 +0530

    Initial commit of test code

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>LankaGuide AI - Test Branch</h1>
+      <p>This is a test commit for my new branch.</p>
+    </div>
+  );
+}
+
+export default App;
